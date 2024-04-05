6872ffbca3b9f9ec276a0947c194cd663cd21270
Catalina <mariacatalina27.nica@gmail.com>
2024-04-05 20:46:42 +0300

    add buchet

--- Proiect/src/main/java/Proiect/Buchet.java
+++ Proiect/src/main/java/Proiect/Buchet.java
@@ -1,4 +1,27 @@
 package Proiect;
 
 public class Buchet {
+
+    private boolean trandafir;
+    private boolean frezie;
+    private boolean garoafa;
+    private boolean bujor;
+    private boolean lalea;
+    private boolean hortensie;
+    private boolean verdeata;
+
+    public Buchet(boolean trandafir, boolean frezie, boolean garoafa, boolean bujor, boolean lalea, boolean hortensie, boolean verdeata) {
+        this.trandafir = trandafir;
+        this.frezie = frezie;
+        this.garoafa = garoafa;
+        this.bujor = bujor;
+        this.lalea = lalea;
+        this.hortensie = hortensie;
+        this.verdeata = verdeata;
+    }
+
+
+
+
+
 }
